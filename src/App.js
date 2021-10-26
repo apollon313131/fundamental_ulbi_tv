32574c74d205a731bee8e4c4f4d56f4c2530f002
apollon313131 <apollo-bel@mail.ru>
2021-10-26 13:50:18 +0300

    step 2

--- src/App.js
+++ src/App.js
@@ -1,53 +1,71 @@
-import React, { useRef, useMemo } from "react";
+import React, { useMemo, useEffect } from "react";
 import { useState } from "react";
+import PostService from "./components/API/PostService";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import Loader from "./components/UI/button/Loader/Loader";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/button/MyModal/MyModal";
+import { useFetching } from "./hooks/useFetching";
+import { usePosts } from "./hooks/usePosts";
 import "./styles/App.css";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Avascript2", body: "ADescription" },
-    { id: 3, title: "Bavascript3", body: "Description" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sertedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
-
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  //   const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
-  //получаем post из дочернего компонента
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  //   async function fetchPosts() {
+  //     setIsPostsLoading(true);
+
+  //     setIsPostsLoading(false);
+  //   }
+
+  //! получаем post из дочернего компонента
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пользователя{" "}
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sertedAndSearchPosts}
-        title="Список постов JS"
-      />
+      {postError && <h1>Error {postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Список постов JS"
+        />
+      )}
     </div>
   );
 }
